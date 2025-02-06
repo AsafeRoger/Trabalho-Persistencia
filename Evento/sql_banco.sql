fce09da4315f27db3c1de871d44d62f5bcd5d28e
Asafe Roger <134728635+AsafeRoger@users.noreply.github.com>
2025-02-06 15:28:31 -0300

    Update sql_banco.sql

--- Evento/sql_banco.sql
+++ Evento/sql_banco.sql
@@ -1,13 +1,13 @@
 CREATE TABLE eventos (
     id INT AUTO_INCREMENT NOT NULL,
-    tipo VARCHAR(20) NOT NULL,  -- Tipo de evento (Aniversario, Casamento)
-    data DATE NOT NULL,  -- Data do evento
-    orcamento DECIMAL(10, 2) NOT NULL,  -- Orçamento do evento
-    qntConvidados INT NOT NULL,  -- Quantidade de convidados
-    local VARCHAR(255) NOT NULL,  -- Local do evento
-    nome_aniversariante VARCHAR(70) DEFAULT NULL,  -- Nome do aniversariante (para Aniversário)
-    tema_festa VARCHAR(70) DEFAULT NULL,  -- Tema da festa (para Aniversário)
-    noivo VARCHAR(70) DEFAULT NULL,  -- Nome do noivo (para Casamento)
-    noiva VARCHAR(70) DEFAULT NULL,  -- Nome da noiva (para Casamento)
+    tipo VARCHAR(20) NOT NULL, 
+    data INT NOT NULL,  
+    orcamento DECIMAL(10, 2) NOT NULL,  
+    qntConvidados INT NOT NULL, 
+    local VARCHAR(255) NOT NULL,  
+    nome_aniversariante VARCHAR(70) DEFAULT NULL,
+    tema_festa VARCHAR(70) DEFAULT NULL,  
+    noivo VARCHAR(70) DEFAULT NULL,  
+    noiva VARCHAR(70) DEFAULT NULL, 
     PRIMARY KEY (id)
 );
